1e052b8c55eb8e3ae6fb3ed33957d4a23d870781
Ramma <ammarshiddiq2@gmail.com>
2025-11-12 09:30:48 +0700

    Tambahan

--- projects/role-lab/routes/web.php
+++ projects/role-lab/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +15,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rute untuk semua user yang terautentikasi
+    Route::get('/all', function () {
+        return view('all');
+    });
+
+    // Rute untuk admin
+    Route::get('/admin', function () {
+        return view('admin');
+    })->middleware('role:admin');
+    
+    // Rute untuk semua user yang terautentikasi
+    Route::get('/manager', function () {
+        return view('manager');
+    })->middleware('role:manager');
+
+    // Rute untuk semua user yang terautentikasi
+    Route::get('/user', function () {
+        return view('user');
+    })->middleware('role:user');
 });
 
 require __DIR__.'/auth.php';
